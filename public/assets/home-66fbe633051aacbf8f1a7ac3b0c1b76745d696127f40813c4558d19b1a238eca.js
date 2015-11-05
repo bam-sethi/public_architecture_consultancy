8e6ea99415632f36a5011cc8e55287ea2551e2f1
Ambar Sethi <ambarrrsethi@gmail.com>
2015-11-05 14:57:31 +0000

    adding pg files

---
+++ public/assets/home-66fbe633051aacbf8f1a7ac3b0c1b76745d696127f40813c4558d19b1a238eca.js
@@ -0,0 +1,23 @@
+// // console.log('scrolllllll')
+// // $(window).ready(function(){
+
+// //   $('div.index-container').scrollLeft(300);
+// //     console.log($(this))
+// // })
+
+
+// $(function() {
+//   $('div.index-container').bind('click',function(event){
+//     var $anchor = $(this);
+    
+//     if you want to use one of the easing effects:
+//     $('html, body').stop().animate({
+//       scrollLeft: $($anchor.attr('href')).offset().left
+//     }, 1500,'easeInOutExpo');
+     
+//     $('html, body').stop().animate({
+//       scrollLeft: $($anchor.attr('href')).offset().left
+//     }, 1000);
+//     event.preventDefault();
+//   });
+// });
---
+++ public/assets/like-38eddc4443e09e5164ece85250fb318e2d61b01dc938975f621d6a0c3f693f03.js
@@ -0,0 +1,28 @@
+console.log('like.js');
+
+//on click of weird like button
+$('img.like-button').on('click', function() {
+  //grabbing the id of suggestion and building from a data attribute
+  var buildingID = $(this).parent().data('building-id')
+  var suggestionID = $(this).parent().data('suggestion-id')
+
+//making a dynamic url grabbed from data attribute set on like button
+  var url = "/buildings/" + buildingID  + "/suggestions/" + suggestionID + "/likes" 
+  var element = $(this);
+  
+//post request, add html to view
+// controller incrementing with each click
+//response is
+  request('POST', url, null).done(function(response){
+    $('li[data-building-id="' + buildingID +  '"][data-suggestion-id="' + suggestionID + '"] .likes').html(response.number_of_likes)
+  })
+})
+
+function request(method, url, data){
+  return $.ajax({
+    method: method,
+    url: url,
+    dataType: 'json',
+    data: data
+  })
+};
